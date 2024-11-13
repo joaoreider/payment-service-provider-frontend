dfbab33ebca8a4737a234a90ae0c866817aa3aec
Joao Paulo <joaopauloj405@gmail.com>
2024-11-13 16:55:02 -0300

    feat: add TransactionsList component

--- src/components/TransactionsList.tsx
+++ src/components/TransactionsList.tsx
@@ -1,12 +1,48 @@
+import { List, Avatar } from 'antd';
+import { AiTwotoneDollarCircle } from 'react-icons/ai';
 
-import { FC } from 'react'
+interface Transaction {
+  id: number;
+  amount: number;
+  status: string;
+  date: string;
+}
 
 interface TransactionsListProps {
-  
+  transactions: Transaction[];
 }
 
-const TransactionsList: FC<TransactionsListProps> = ({  }) => {
- return <div> TransactionsList </div>
-}
+const TransactionsList= ({ transactions }: TransactionsListProps) => {
+  return (
+    <div className='overflow-y-scroll w-1/2 min-w-[600px] max-h-96 p-4 mx-6 mt-4'>
+      <List
+      className='shadow-lg rounded-lg'
+        itemLayout="horizontal"
+        dataSource={transactions}
+        renderItem={transaction => (
+          <List.Item className="flex justify-center">
+            <List.Item.Meta
+              avatar={
+                <Avatar size={40} style={{ backgroundColor: 'transparent' }}>
+                    <AiTwotoneDollarCircle size={20}/>
+                </Avatar>
+              }
+              title={
+                <span className={transaction.status === 'paid' ? 'text-green-500' : 'text-orange-500'}>
+                  ${transaction.amount.toFixed(2)}
+                </span>
+              }
+              description={
+                <>
+                  <span className='font-bold'>{transaction.status === 'paid' ? 'Paid' : 'Waiting Funds'}</span> - {transaction.date}
+                </>
+              }
+            />
+          </List.Item>
+        )}
+      />
+    </div>
+  );
+};
 
-export default TransactionsList
+export default TransactionsList;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,13 +2,24 @@ import { Button } from 'antd';
 import BalanceCard from '../components/BalanceCard';
 import PageTitle from '../components/PageTitle';
 import { AiFillPlusSquare } from "react-icons/ai";
+import TransactionsList from '../components/TransactionsList';
 
+const transactions = [
+  { id: 1, amount: 300, status: 'paid', date: 'August 14, 2024' },
+  { id: 2, amount: 200, status: 'waiting', date: 'August 15, 2024' },
+  { id: 3, amount: 300, status: 'paid', date: 'August 14, 2024' },
+  { id: 4, amount: 200, status: 'waiting', date: 'August 15, 2024' },
+  { id: 5, amount: 300, status: 'paid', date: 'August 14, 2024' },
+  { id: 6, amount: 200, status: 'waiting', date: 'August 15, 2024' },
+  { id: 7, amount: 300, status: 'paid', date: 'August 14, 2024' },
+  { id: 8, amount: 200, status: 'waiting', date: 'August 15, 2024' },
+];
 
 const Home = () => {
   return (
     <>
       <PageTitle title="Home | PSP" />
-      <div className='flex flex-col items-center min-h-screen bg-blue-100'>
+      <div className='flex flex-col items-center min-h-screen bg-blue-100 px-80'>
 
         <div className="flex justify-center my-20">
             <div className="flex space-x-36">
@@ -17,6 +28,8 @@ const Home = () => {
             </div> 
           </div>
         <Button variant='outlined' className='flex justify-center' icon={<AiFillPlusSquare />} size='large'>Create Transaction</Button>
+        <p className='text-lg mt-8 text-gray-500  w-1/2 '>History</p>
+        <TransactionsList transactions={transactions} />
 
       </div>
  
